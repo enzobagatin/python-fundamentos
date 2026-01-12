43346853a945b94260ec2c82661d0471a0a61586
Enzo Bagatin Michelao <enzobagatin@gmail.com>
2026-01-12 08:53:35 -0300

    03_loops.py

---
+++ 03_loops.py
@@ -0,0 +1,13 @@
+# Estruturas de repetição em Python
+
+lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+soma = 0
+for n1 in lista1:
+  soma += n1
+
+media = soma / len(lista1)
+print("Média: ", media)
+
+for i in range(11):
+  print("Número: ", i)
