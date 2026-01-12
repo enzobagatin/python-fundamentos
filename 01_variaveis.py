6188841c2aca1dc1a53d722b48ed6b30d2ae3e95
Enzo Bagatin Michelao <enzobagatin@gmail.com>
2026-01-12 08:03:49 -0300

    01_variaveis.py

---
+++ 01_variaveis.py
@@ -0,0 +1,16 @@
+# Variáveis e tipos básicos em Python
+
+nome = "Enzo"
+idade = 20
+altura = 1.75
+estudante = True
+
+print(nome)
+print(idade)
+print(altura)
+print(estudante)
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(estudante))
